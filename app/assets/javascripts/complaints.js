3280ccc5afdfa0dffaf97e2ae502eb82491d3029
leo-holanda <leo.uldholanda@gmail.com>
2020-03-10 15:38:38 -0300

    Fix multiline string in panel and assets

--- app/assets/javascripts/complaints.js
+++ app/assets/javascripts/complaints.js
@@ -17,15 +17,15 @@ var sidebar = L.control.sidebar({
 sidebar.addPanel({
   id: "app_info", // UID, used to access the panel
   tab: '<i class="fas fa-home"></i>', // content can be passed as HTML string,
-  pane: `<br><p class="is-family-primary has-text-weight-semibold is-size-6"> 
-         Noise-map é mapa interativo de relatos sobre ruídos urbanos como obras de construção civil, tráfego de veículos e vizinhos incômodos, por exemplo. 
-         É possível visualizar os relatos através de marcadores no mapa que, ao serem clicados, revelam o tipo de ruído, a descrição proveniente do usuário e
-         o dia e hora de criação do relato. <br><br> Caso haja uma área com alta concentração de relatos, estes serão agrupados para facilitar seu acesso, bem como para
-         melhorar a visibilidade do mapa. Nota-se o agrupamento através de um círculo colorido envolvendo um contador de relatos que, ao ser clicado, revela os marcadores anteriormente agrupados. 
-         Além disso, é possível visualizar os relatos através do mapa de calor clicando no ícone <i class="fas fa-layer-group"></i>.
-         <br><br> Para adicionar um relato, clique no ícone <i class="fas fa-plus-circle"></i>. Para visualizar os relatos nos arredores de um determinado endereço,
-         clique no ícone <i class="fas fa-search"></i>. 
-         </p>`, // DOM elements can be passed, too
+  pane: '<br><p class="is-family-primary has-text-weight-semibold is-size-6"> \
+         Noise-map é mapa interativo de relatos sobre ruídos urbanos como obras de construção civil, tráfego de veículos e vizinhos incômodos, por exemplo. \
+         É possível visualizar os relatos através de marcadores no mapa que, ao serem clicados, revelam o tipo de ruído, a descrição proveniente do usuário e \
+         o dia e hora de criação do relato. <br><br> Caso haja uma área com alta concentração de relatos, estes serão agrupados para facilitar seu acesso, bem como para \
+         melhorar a visibilidade do mapa. Nota-se o agrupamento através de um círculo colorido envolvendo um contador de relatos que, ao ser clicado, revela os marcadores anteriormente agrupados. \
+         Além disso, é possível visualizar os relatos através do mapa de calor clicando no ícone <i class="fas fa-layer-group"></i>. \
+         <br><br> Para adicionar um relato, clique no ícone <i class="fas fa-plus-circle"></i>. Para visualizar os relatos nos arredores de um determinado endereço, \
+         clique no ícone <i class="fas fa-search"></i>. \
+         </p>', // DOM elements can be passed, too
   title: "Mapa de Ruído Urbano", // an optional pane header
   position: "top"
 });
